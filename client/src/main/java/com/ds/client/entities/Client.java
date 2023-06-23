a11a83f96ab2b06f44086ffa1b7ddcca0242290e
jl-do <doerzbacherjiaine@gmaill.com>
2023-06-23 14:33:33 -0300

    Client class

---
+++ client/src/main/java/com/ds/client/entities/Client.java
@@ -0,0 +1,95 @@
+package com.ds.client.entities;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+
+    private Instant birthDate;
+    private Integer children;
+
+    public Client() {
+    }
+    
+    public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public Double getIncome() {
+        return this.income;
+    }
+    
+    public Instant getBirthDate() {
+        return this.birthDate;
+    }
+
+    public Integer getChildren() {
+        return this.children;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+    
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Client client)) return false;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
